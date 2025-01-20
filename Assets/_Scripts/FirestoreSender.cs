725d17927da1931e662f60e5454753ea67a27abe
Andres Urrego <andres.urrego@polygon.us>
2025-01-20 11:38:56 -0500

    - use compatc firebase-webgl Bridge

--- Assets/_Scripts/FirestoreSender.cs
+++ Assets/_Scripts/FirestoreSender.cs
@@ -1,24 +1,28 @@
+using FirebaseWebGL.Scripts.FirebaseBridge;
 using System;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
-using UnityEngine.Networking;
 
 public class FirestoreSender : MonoBehaviour
 {
     public static Action<SwipeDirection> directionToSend;
 
     [SerializeField, TextArea] private string baseUrl;
-
-    private const string _put = "PUT";
-
-    private readonly Dictionary<SwipeDirection, byte[]> values = new()
+    private readonly Dictionary<SwipeDirection, int> values = new()
     {
-        { SwipeDirection.None, new byte[] { 48 } },
-        { SwipeDirection.Up, new byte[] { 49 } },
-        { SwipeDirection.Down, new byte[] { 50 } },
-        { SwipeDirection.Left, new byte[] { 51 } },
-        { SwipeDirection.Right, new byte[] { 52 } },
+        { SwipeDirection.None, 0 },
+        { SwipeDirection.Up, 1 },
+        { SwipeDirection.Down, 2 },
+        { SwipeDirection.Left, 3 },
+        { SwipeDirection.Right, 4 },
     };
+
+    private const string _document = "A1B1";
+
+    private int counter = 0;
+
+    private readonly StringBuilder _jsonBuilder = new(64);
 
     private void Awake()
     {
@@ -32,11 +36,16 @@ public class FirestoreSender : MonoBehaviour
 
     private void SendDirection(SwipeDirection direction)
     {
-        UnityWebRequest webRequest = new(baseUrl, _put)
-        {
-            uploadHandler = new UploadHandlerRaw(values[direction])
-        };
-
-        webRequest.SendWebRequest();
+        FirebaseDatabase.UpdateJSON(_document, GetJson(values[direction], counter++));
+    }
+    private string GetJson(int direction, int count)
+    {
+        _jsonBuilder.Clear();
+        _jsonBuilder.Append("{\"direction\": ");
+        _jsonBuilder.Append(direction);
+        _jsonBuilder.Append(", \"count\": ");
+        _jsonBuilder.Append(count);
+        _jsonBuilder.Append("}");
+        return _jsonBuilder.ToString();
     }
 }
